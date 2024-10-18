8798cf7cc226db12b91bd02b1b6486139e9e4677
Sergey Shulga <shul.sergio@gmail.com>
2024-10-18 21:10:35 +0300

    v 1.2 add quiz component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
-
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import Loader from "./components/Loader/Loader";
+import Quiz from "./components/Quiz/Quiz";
 
 function App() {
+  const [loading, isLoading] = useState(false);
 
   return (
     <>
       <h1>QUIZ</h1>
+      {loading && <Loader />}
+      <Quiz/>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Quiz/Quiz.jsx
+++ src/components/Quiz/Quiz.jsx
@@ -1,4 +1,15 @@
+import Question from "../Question/Question";
+import css from "./Quiz.module.css";
+
 const Quiz = () => {
-  return <></>;
+  return (
+    <>
+      <div className={css.quiz}>
+        <div className={css.score}>Question 1/10</div>
+        <Question />
+        <div className={css.nextBtn}>Next</div>
+      </div>
+    </>
+  );
 };
 export default Quiz;
